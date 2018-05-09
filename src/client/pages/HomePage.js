4e433872c8122d7707de61e84fc6af22e5171acc
Arup Rakshit <aruprakshit@rocketmail.com>
2018-05-09 22:17:59 +0530

    add authentication

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React, {} from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div>I am in home component</div>
-      <button onClick={() => console.log('Hi there!')}>Click ME!</button>
+    <div className="center-align" style={{"marginTop": "200px"}}>
+      <h3>Welcome!</h3>
+      <p>Checkout this awesome features</p>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import "babel-polyfill";
-import  express from 'express';
+import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
+import proxy from "express-http-proxy";
 import Routes from "./client/Routes";
 import { matchRoutes } from "react-router-config";
 
 const app = express();
 
+app.use('/api', proxy('https://react-ssr-api.herokuapp.com/', {
+  proxyReqOptDecorator(opts) {
+    opts.headers['x-forwarded-host'] = "localhost:3000";
+    return opts;
+  }
+}));
+
 app.use(express.static('public'));
 
 app.get('*', (req, resp) => {
-  const store = createStore();
-  const promises = matchRoutes(Routes, req.path).map(({route}) => {
+  const store = createStore(req);
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
   });
-  
-  Promise.all(promises).then(() => {    
+
+  Promise.all(promises).then(() => {
     resp.send(renderer(req, store));
   });
 });
@@ -24,4 +32,4 @@ app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
-// completed till 047
+// completed till 064
